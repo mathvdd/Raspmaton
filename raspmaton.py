832255c7dbdcfbc1784b8b2b0722157d56726124
mathvdd <mathieu@mathvdd.com>
2022-06-13 22:07:28 +0200

    test lazysizes

--- raspmaton.py
+++ raspmaton.py
@@ -137,7 +137,7 @@ while True:
                 try: # lazy load the other images
                     file_count = int(filename[-8:-4])
                     content += '''<div class="imgbox">
-                        <img class="center-fit lazyload" loading="lazy" src='{}'>
+                        <img class="center-fit" loading="lazy" src='{}'>
                     </div>'''.format(drive_name + path_file.split(path_drive)[1])
                 except:
                     pass 
