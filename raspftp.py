e5ec612c6d62ff1d4a8a76a2459e0d09e8145dbd
mathvdd <mathieu@mathvdd.com>
2022-08-03 20:46:19 +0200

    update raspftp

--- raspftp.py
+++ raspftp.py
@@ -5,7 +5,7 @@ https://github.com/mathvdd/Raspmaton
 '''
 
 from ftplib import FTP
-import os
+
 
 with open("parameters.txt") as f:
     param = dict(i.rstrip().split(':') for i in f if i.startswith('#') == False)
@@ -13,37 +13,18 @@ with open("parameters.txt") as f:
 def connect():
     ftp = FTP(param.get('domain'))
     ftp.login(user=param.get('username'), passwd = param.get('password'))
-    ftp.cwd(param.get('ftp_www'))
     return ftp
 
-def check_content(ftp_object, subdir):
+def check_content(ftp_object):
     # check if the remote directory exists and create it
-    #if len(ftp.nlst(param.get('ftp_www'))) == 0:
-    #    ftp.mkd(param.get('ftp_www'))
+    if len(ftp.nlst(param.get('ftp_www'))) == 0:
+        ftp.mkd(param.get('ftp_www'))
     # go into the directory
-    #ftp.cwd(param.get('ftp_www'))
-    #ftp.retrlines('LIST')
-    return ftp.nlst(subdir)
+    ftp.cwd(param.get('ftp_www'))
+    ftp.retrlines('LIST')
+    
 
-def upload_content(ftp, subdir):
-   remote_list = ftp.nlst(subdir)
-   local_list = os.listdir(os.path.join('/home/raspmaton/www/USBdrive', subdir))
-   for im in local_list:
-      if im not in remote_list:
-         ftp.storbinary('STOR '+os.path.join(subdir,im), open(os.path.join('/home/raspmaton/www/USBdrive', subdir, im), 'rb'))
-
-def update_index(ftp, local_dir):
-   ftp.storbinary('STOR '+'index.html', open(os.path.join(local_dir,'raspmaton.html'), 'rb'))
-   #ftp.storbinary('STOR '+'lazysizes.min.js', open(os.path.join(local_dir,'lazysizes.min.js'), 'rb'))
-def disconnect(ftp):
-    ftp.quit()
-
-if __name__ == "__main__":
-   # filename = 'qr.svg'
-   # ftp.storbinary('STOR '+filename, open(filename, 'rb'))
-   ftp = connect()
-   #remote_filelist = check_content(ftp, 'noname')
-   #print(remote_filelist)
-   upload_content(ftp, 'noname')
-   update_index(ftp, '/home/raspmaton/www')
-   disconnect(ftp)
+# filename = 'qr.svg'
+# ftp.storbinary('STOR '+filename, open(filename, 'rb'))
+ftp = connect()
+ftp.quit()
